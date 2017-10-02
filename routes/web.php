c8d80d22f4732a153539a5afe09badde19126771
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-10-02 22:59:18 +0300

    blog ve blog_post blade leri değiştirildi...

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,12 @@
 
 Route::get('/', 'Front@index');
 Route::get('/products', 'Front@products');
-Route::get('/products/details/{id}', 'Front@products_details');
-Route::get('/products/categories/', 'Front@products_categories');
-Route::get('/products/brands', 'Front@products_brands');
+Route::get('/products/details/{id}', 'Front@product_details');
+Route::get('/products/categories/{name}', 'Front@product_categories');
+Route::get('/products/brands/{name}/{category?}', 'Front@product_brands');
 Route::get('/blog', 'Front@blog');
-Route::get('/blog/post/{id}', 'Front@blog_post');
+Route::get('/blog/{id}', 'Front@blog_post');
 Route::get('/contact-us', 'Front@contact_us');
-Route::get('/login', 'Front@login');
-Route::get('/logout', 'Front@logout');
-Route::get('/cart', 'Front@cart');
-Route::post('/cart', 'Front@cart');
-Route::get('/checkout', [
-    'middleware' => 'auth',
-    'uses' => 'Front@checkout'
-]);
-Route::get('/search/{query}', 'Front@search');
 // Authentication routes...
 Route::get('auth/login', 'Front@login');
 Route::post('auth/login', 'Front@authenticate');
@@ -36,13 +27,23 @@ Route::get('auth/logout', 'Front@logout');
 // Registration routes...
 Route::post('/register', 'Front@register');
 
+// Registration routes...
+Route::get('/cart', 'Front@cart');
+Route::post('/cart', 'Front@cart');
+Route::post('/cart-remove-item', 'Front@cart_remove_item');
+Route::get('/clear-cart', 'Front@clear_cart');
+Route::get('/checkout', [
+    'middleware' => 'auth',
+    'uses' => 'Front@checkout'
+]);
+Route::get('/search/{query}', 'Front@search');
 
 // API routes...
 Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id = null) {
     if ($id == null) {
-        $products = App\Product::all(['id', 'name', 'price']);
+        $products = App\Product::all(array('id', 'name', 'price'));
     } else {
-        $products = App\Product::find($id, ['id', 'name', 'price']);
+        $products = App\Product::find($id, array('id', 'name', 'price'));
     }
     return Response::json(array(
         'error' => false,
@@ -53,9 +54,9 @@ Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id
 
 Route::get('/api/v1/categories/{id?}', ['middleware' => 'auth.basic', function($id = null) {
     if ($id == null) {
-        $categories = App\Category::all(['id', 'name']);
+        $categories = App\Category::all(array('id', 'name'));
     } else {
-        $categories = App\Category::find($id, ['id', 'name']);
+        $categories = App\Category::find($id, array('id', 'name'));
     }
     return Response::json(array(
         'error' => false,
